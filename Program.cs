959e99faa65027a2bba583e7a386e1ac70cb546b
Taylor Rodieck <tw.rodieck@gmail.com>
2022-06-07 09:45:20 -0500

    Refactoring and quality of life upgrades.

--- Program.cs
+++ Program.cs
@@ -5,26 +5,64 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data;
+using System.Threading;
 using System.Data.SqlClient;
 using System.Net;
 using System.Net.Http;
 using ChoETL;
+using System.Windows.Forms;
 
 namespace CSV_Merge
 {
     internal class Program
     {
+        [STAThread]
         static void Main(string[] args)
         {
-            ChoCSVRecordConfiguration config = null;
+            Console.WriteLine("Select First CSV File...");
+            Thread.Sleep(2000);
+            OpenFileDialog oFD_1 = new OpenFileDialog();
+            oFD_1.ShowDialog();
+            string pathOne = Path.GetFullPath(oFD_1.FileName);
+            Console.Write("Path One: " + pathOne);
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.WriteLine("Select Second CSV File...");
+            Thread.Sleep(2000);
+            OpenFileDialog oFD_2 = new OpenFileDialog();
+            oFD_2.ShowDialog();
+            string pathTwo = Path.GetFullPath(oFD_2.FileName);
+            Console.Write("Path Two: " + pathTwo);
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.WriteLine("Enter the name of the result file...");
+            string resultFileName = Path.GetDirectoryName(pathTwo) + "\\" + Console.ReadLine() + ".csv";
+            Console.Write("Result Path: " + resultFileName);
+            Console.WriteLine();
+            Console.WriteLine();
+            //Console.WriteLine("pathOne: " + pathOne); // Debugging Purposes Only
+            //Console.WriteLine("pathTwo" + pathTwo); // Debugging Purposes Only
+            //Console.WriteLine("resultFileName" + resultFileName); // Debugging Purposes Only
+
+            Csv_Merger(pathOne, pathTwo, resultFileName); // Invoke Merger Method
+
+            Console.WriteLine("Done...");
+            Console.ReadKey();
+        }
+
+        static void Csv_Merger(string pathOne, string pathTwo, string resultFileName)
+        {
+            ChoCSVRecordConfiguration config = null; // Initialize config settings
 
             List<object> items = new List<object>();
 
-            foreach (var file in Directory.GetFiles(@"X:\VisualStudioProjectsTesting\CSV_Operations", "*.csv", SearchOption.TopDirectoryOnly))
+            foreach (var file in Directory.GetFiles(Path.GetDirectoryName(pathOne), "*.csv", SearchOption.TopDirectoryOnly))
             {
                 using (var r1 = new ChoCSVReader(Path.GetFullPath(file)).WithFirstLineHeader())
                 {
-                    items.Add(r1.First());
+                    items.Add(r1.First()); // Adds CSV as object to list
                 }
             }
 
@@ -32,26 +70,20 @@ namespace CSV_Merge
             using (var w = new ChoCSVWriter(csv).WithFirstLineHeader().WithMaxScanRows(5).ThrowAndStopOnMissingField(false))
             {
                 w.Write(items);
-                //Capture configuration for later use to merge CSV files
+                // Only used to get configuration for these files
                 config = w.Configuration;
             }
 
-            using (var r1 = new ChoCSVReader(@"X:\VisualStudioProjectsTesting\CSV_Operations\test1.csv").WithFirstLineHeader())
+            using (var r1 = new ChoCSVReader(pathOne).WithFirstLineHeader())
             {
-                using (var r2 = new ChoCSVReader(@"X:\VisualStudioProjectsTesting\CSV_Operations\test2.csv").WithFirstLineHeader())
+                using (var r2 = new ChoCSVReader(pathTwo).WithFirstLineHeader())
                 {
-                    using (var w1 = new ChoCSVWriter(@"X:\VisualStudioProjectsTesting\CSV_Operations\combined.csv", config).WithFirstLineHeader())
+                    using (var w1 = new ChoCSVWriter(resultFileName, config).WithFirstLineHeader())
                     {
-                        w1.Write(r1.Concat(r2));
-                        //Console.WriteLine(w1.ToString());
-
-
+                        w1.Write(r1.Concat(r2)); // Writes the result of concatenating 
                     }
                 }
             }
-
-            Console.WriteLine("Done...");
-            Console.ReadKey();
         }
     }
 }

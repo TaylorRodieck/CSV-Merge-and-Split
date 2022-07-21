641b9bee38bd9f6c2743a7ff4695e37c36f99105
Taylor Rodieck <tw.rodieck@gmail.com>
2022-07-21 14:29:15 -0500

    Added a 'split' functionality. Can also do another process within the same session.

--- Program.cs
+++ Program.cs
@@ -7,12 +7,43 @@ using System.Threading;
 using ChoETL;
 using System.Windows.Forms;
 
-namespace CSV_Merge
+namespace CSV_MergeAndSplit
 {
     internal class Program
     {
         [STAThread]
         static void Main(string[] args)
+        {
+            InitialSetup();
+            Console.WriteLine("To Process Another File, Press 'F'...");
+            string reProcess = Console.ReadLine();
+            if (reProcess == "F")
+            {
+                InitialSetup();
+            }
+
+            Console.WriteLine("Done...");
+            Console.ReadKey();
+        }
+
+        static void InitialSetup()
+        {
+            Console.WriteLine("Enter 'SPLIT' to split a csv and 'MERGE' to merge two CSV's...");
+            string choice = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine();
+
+            if (choice.ToLower() == "split")
+            {
+                csvDocument.splitCSV();
+            }
+            else if (choice.ToLower() == "merge")
+            {
+                CSV_MergerSetUp();
+            }
+        }
+
+        static void CSV_MergerSetUp()
         {
             Console.WriteLine("Select First CSV File...");
             Thread.Sleep(750);
@@ -42,20 +73,20 @@ namespace CSV_Merge
             //Console.WriteLine("resultFileName" + resultFileName); // Debugging Purposes Only
 
             Csv_Merger(pathOne, pathTwo, resultFileName); // Invoke Merger Method
-
-            Console.WriteLine("Done...");
-            Console.ReadKey();
         }
 
+        [STAThread]
         static void Csv_Merger(string pathOne, string pathTwo, string resultFileName)
         {
-            ChoCSVRecordConfiguration config = null; // Initialize config settings
+            //ChoCSVRecordConfiguration config = null; // Initialize config settings
+            ChoCSVRecordConfiguration config = new ChoCSVRecordConfiguration();
+            
 
             List<object> items = new List<object>();
 
             foreach (var file in Directory.GetFiles(Path.GetDirectoryName(pathOne), "*.csv", SearchOption.TopDirectoryOnly))
             {
-                using (var r1 = new ChoCSVReader(Path.GetFullPath(file)).WithFirstLineHeader())
+                using (var r1 = new ChoCSVReader(Path.GetFullPath(file)).WithFirstLineHeader().ThrowAndStopOnMissingField(false))
                 {
                     items.Add(r1.First()); // Adds CSV as object to list
                 }
@@ -76,6 +107,58 @@ namespace CSV_Merge
                     using (var w1 = new ChoCSVWriter(resultFileName, config).WithFirstLineHeader())
                     {
                         w1.Write(r1.Concat(r2)); // Writes the result of concatenating 
+                    }
+                }
+            }
+        }
+
+        public class csvDocument
+        {
+            public string FilePath;
+            public bool HasHeaders;
+            public int RowSplitCount;
+            private int TotalRowCount;
+
+            public csvDocument(string filePath, bool hasHeaders, int rowSplitCount, int totalRowCount = 0)
+            {
+                FilePath = filePath;
+                HasHeaders = hasHeaders;
+                RowSplitCount = rowSplitCount;
+                TotalRowCount = totalRowCount;
+            }
+
+            public static void splitCSV()
+            {
+                OpenFileDialog ofd_1 = new OpenFileDialog();
+                ofd_1.ShowDialog();
+                string filePath = ofd_1.FileName;
+                Console.WriteLine("Path of Selected File to be Split :: " + filePath);
+
+                Console.WriteLine();
+                Console.Write("Row Limit Per File :: ");
+                int rowLimit = int.Parse(Console.ReadLine());
+
+                //string filePath = csvDoc.FilePath;
+                //int rowLimit = csvDoc.RowSplitCount;
+                using (System.IO.StreamReader sr = new System.IO.StreamReader(filePath))
+                {
+                    int fileNumber = 0;
+
+                    while (!sr.EndOfStream)
+                    {
+                        int count = 0;
+                        //Console.WriteLine(Directory.GetParent(filePath) + "\\" + Path.GetFileNameWithoutExtension(filePath) + "_Split_" + ++fileNumber + ".csv");
+
+                        using (System.IO.StreamWriter sw = new System.IO.StreamWriter(Directory.GetParent(filePath) + "\\" + Path.GetFileNameWithoutExtension(filePath) + "_Split_" + ++fileNumber + ".csv"))
+                        {
+                            sw.AutoFlush = true;
+
+                            while (!sr.EndOfStream && ++count < rowLimit)
+                            {
+                                sw.WriteLine(sr.ReadLine());
+
+                            }
+                        }
                     }
                 }
             }
